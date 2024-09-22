5dfc4158e6e866c9d4ea36e063d87e2806846e34
nfphys <fumiya-nakamura@c-fo.com>
2024-09-22 16:23:46 +0900

    add templates

---
+++ templates/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(r io.Reader, w io.Writer) {
+	sc := bufio.NewScanner(r)
+	sc.Split(bufio.ScanWords)
+
+	scanInt := func() int {
+		sc.Scan()
+		i, err := strconv.Atoi(sc.Text())
+		if err != nil {
+			panic(err)
+		}
+		return i
+	}
+
+	// solve here
+}
