889b24d6201bc952085c89ed55ee125efb88c0d6
nfphys <fumiya-nakamura@c-fo.com>
2024-10-14 15:36:28 +0900

    solve ABC375 A

---
+++ contests/abc375/a/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+type Case struct {
+	Input string
+	Want string
+}
+
+func TestSolve(t *testing.T) {
+	// add cases here
+	cases := []Case{
+		{
+			`6
+			#.##.#
+			`,
+			"2",
+		},
+		{
+			`1
+			#
+			`,
+			"0",
+		},
+		{
+			`9
+			##.#.#.##
+			`,
+			"3",
+		},
+	}
+
+	for _, c := range cases {
+		r := strings.NewReader(c.Input)
+		var w strings.Builder
+
+		solve(r, &w)
+
+		got := strings.Trim(w.String(), "\n ")
+
+		if c.Want != got {
+			t.Fatalf("want %s, got %s", c.Want, got)
+		}
+	}
+}
